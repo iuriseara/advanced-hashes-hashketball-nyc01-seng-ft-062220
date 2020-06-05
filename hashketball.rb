aeeb1f9502c5d9b7d7491745db027ea895089081
fearless-command-2358 <iuriseara@gmail.com>
2020-06-05 22:03:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,7 +196,7 @@ def player_stats(name)
   game_hash.each do |key, value|
     value[:players].each do |player|
       if player[:player_name] == name
-        new_hash = player.delete_if {|stat_key, stat_value}| [:player_name].include?(stat_key)}
+        new_hash = player.delete_if {|stat_key, stat_value| [:player_name].include?(stat_key)}
         new_hash[:player_name] = name
         return new_hash
       end
